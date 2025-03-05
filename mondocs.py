6c24cef6c80528368bb634b7a2f21225f4cb0d40
Cody <chodywhooch@live.ca>
2025-03-04 22:27:31 -0700

    Update mondocs.py

--- mondocs.py
+++ mondocs.py
@@ -1,53 +1,10 @@
-"""
----channel_doc---
---data_counters--
-ats: TractorCrashes, GameCrashes
-cod: TotalMatches, TotalWins, TotalLosses, TotalCrashes
-stream_crash: TotalCrashes
---data_games--
-ranword: Random_Word
-tag: user_id, user_name, time tagged
-
----user_doc---
---data_games--
-fish: Dict
-    auto: Dict
-        cast: AutoCastLeft Float
-        cost: AutoCastCost Float
-        gain: TotalGained Float
-        lost: TotalLost Float
-        rewards: Rewards List
-    line: Dict
-        cast: CurrentlyCasted Bool
-        cut: Next/CurrentCastCut Bool
-        cut_by: If^^NameOfChatterWhoDidIt Str
-        cut_last: LastCut Datetime
-iq: Dict
-    current: CurrentIQ Int
-    last: LastChecked Datetime
-    history: HistoryIQ List
-tag: [Total/Good/Fail]
-pp: [Size/LastDone/[History]]
---data_rank--
-id: user_id
-login: user_login
-discord_id: discord_user_id -- to be phased out?
-points: points_earned
--dates-
-first_chat: FirstChat
-latest_chat: LastChat
-checkin_streak: TimesCheckedIn(ResetOn5), LastCheckInDate
--channel-
-id: broadcaster_id_document_created_with
-name: broadcaster_name_document_created_with
-"""
-from mongoengine import Document, IntField, DynamicField, DictField, StringField
+from mongoengine import Document, DictField, StringField
 
 
 class Channels(Document):
-    _id = IntField(primary_key=True)
-    user_name = DynamicField(default="")
-    user_login = DynamicField(default="")
+    _id = StringField(primary_key=True)
+    user_name = StringField(default="")
+    user_login = StringField(default="")
     channel_details = DictField(default={
         "online": False,
         "online_last": None,
@@ -71,22 +28,46 @@ class Channels(Document):
         "writing_clock": False
     })
     data_counters = DictField(default={
-        "ats": [0, 0],
-        "cod": [0, 0, 0, 0],
-        "joints": [0, None],
-        "stream_crash": 0
+        "ats": {
+            "game_crash": 0,
+            "tractor_crash": 0
+        },
+        "cod": {
+            "game_total": 0,
+            "game_win": 0,
+            "game_loss": 0,
+            "game_crash": 0
+        },
+        "joints": {
+            "smoked": 0,
+            "smoked_last": None,
+            "smoked_history": {}
+        },
+        "stream": {
+            "bot_restart": 0,
+            "crash": 0
+        }
     })
     data_games = DictField(default={
         "fish_recast": [],
+        "gamble": {
+            "total": 10000.0,
+            "viewers": {}
+        },
         "ranword": "",
-        "tag": [None, None, None]
+        "tag": {
+            "tagged_id": None,
+            "tagged_name": None,
+            "tagged_last": None
+        }
     })
     data_lists = DictField(default={
         "ignore": [],
         "lurk": [],
         "mods": [],
         "non_tag": [],
-        "spam": []
+        "spam": [],
+        "coupons": []
     })
 
 
@@ -94,19 +75,86 @@ class Users(Document):
     _id = StringField(primary_key=True)
     name = StringField(default="")
     data_games = DictField(default={
+        "fight": {
+            # "streak": {
+            #     "last": None,  # ToDo: Add Streak Counter For Win/Loss/Tied
+            # },
+            "times_aggressor": {
+                "lost": 0,
+                "tied": 0,
+                "won": 0,
+                "points_lost": 0.0,
+                "points_won": 0.0
+            },
+            "times_defender": {
+                "lost": 0,
+                "tied": 0,
+                "won": 0,
+                "points_lost": 0.0,
+                "points_won": 0.0
+            },
+            "shield": [None, 0],
+            "weapon": [None, 0]
+        },
         "fish": {
             "auto": {
                 "cast": 0,
+                "catches": {},
                 "cost": 0,
-                "gain": 0.0,
-                "lost": 0.0,
-                "rewards": []
             },
             "line": {
                 "cast": False,
+                "caught_last": [],
                 "cut": False,
                 "cut_by": "",
-                "cut_last": None
+                "cut_last": None,
+                "stats": {
+                    "cost": 0,
+                    "effect": 0,
+                    "level": 0,
+                    "name": "Standard"
+                }
+            },
+            "lure": {
+                "cost": 0,
+                "effect": 0,
+                "level": 0,
+                "name": "Standard",
+                "pLow": 0,
+                "pHigh": 100
+            },
+            "reel": {
+                "cost": 0,
+                "effect": 0,
+                "level": 0,
+                "name": "Standard",
+                "pLow": 0,
+                "pHigh": 100
+            },
+            "rod": {
+                "cost": 0,
+                "effect": 0,
+                "level": 0,
+                "name": "Standard",
+                "pLow": 0,
+                "pHigh": 100
+            },
+            "special": {
+                "ice": 0,
+                "lube": 0
+            },
+            "totals": {
+                "auto": {
+                    "catches": {},
+                    "cost": 0.0
+                },
+                "line": {
+                    "cut_by": {},
+                    "cut_other": {}
+                },
+                "manual": {
+                    "catches": {}
+                }
             }
         },
         "iq": {
@@ -115,28 +163,90 @@ class Users(Document):
             "history": []
         },
         "jail": {
+            "attempt_last": None,
+            "escapes": 0,
+            "history": {
+                "attempt_fail": {},
+                "attempt_success": {},
+                "attempt_shielded": {},
+                "early_released": 0,
+                "escaped_attempt": {},
+                "fished": 0,
+                "in": {},
+                "shielded_attempt": {},
+                "times_shielded": 0
+            },
             "in": False,
             "last": None,
-            "escapes": 0
+            "shield": None
         },
-        "tag": [0, 0, 0],
-        "pp": [None, None, []]
-    })
-    data_rank = DictField(default={
-        "boost": 0.0,
-        "level": 1,
-        "xp": 0.0
+        "other": {
+            "bite": {
+                "times_bite": 0,
+                "times_bit": 0
+            },
+            "burn": {
+                "times_burn": 0,
+                "times_burned": 0
+            },
+            "kick": {
+                "times_kick": 0,
+                "times_kicked": 0
+            },
+            "lick": {
+                "times_lick": 0,
+                "times_licked": 0
+            },
+            "pants": {
+                "times_pants": 0,
+                "times_pantsed": 0
+            },
+            "pinch": {
+                "times_pinch": 0,
+                "times_pinched": 0
+            },
+            "pounce": {
+                "times_pounce": 0,
+                "times_pounced": 0
+            },
+            "punch": {
+                "times_punch": 0,
+                "times_punched": 0
+            },
+            "slap": {
+                "times_slap": 0,
+                "times_slapped": 0
+            },
+            "tickle": {
+                "times_tickle": 0,
+                "times_tickled": 0
+            }
+        },
+        "pp": {
+            "size": 0,
+            "last": None,
+            "history": []
+        },
+        "tag": {
+            "total": 0,
+            "success": 0,
+            "fail": 0
+        }
     })
     data_user = DictField(default={
         "id": "",
         "login": "",
-        "discord_id": "",
-        "points": 0.0,
+        "rank": {
+            "boost": 0.0,
+            "level": 1,
+            "points": 0.0,
+            "xp": 0.0,
+        },
         "dates": {
             "first_chat": None,
             "latest_chat": None,
             "checkin_streak": [0, None],
-            "daily_cards": [0, None]
+            "daily_cards": [0, None, []]
         },
         "channel": {
             "id": "",
